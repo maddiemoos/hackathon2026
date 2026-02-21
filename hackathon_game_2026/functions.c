5db2ef878d0f2f5d6a9cc36ce7cab1dd303fb8ca
erasch1506 <emily.rasch@wsu.edu>
2026-02-21 14:02:37 -0800

    added header name to functions file

--- hackathon_game_2026/functions.c
+++ hackathon_game_2026/functions.c
@@ -1,1 +1,28 @@
 #include "header.h"
+
+
+void loadQuestions(Question bank[], int* count) {
+
+
+
+}
+void gamePlay(Question bank[], int count) {
+
+
+
+}
+void editQuestion(Question bank[], int count) {
+
+
+
+}
+void addQuestion(Question bank[], int* count) {
+
+
+
+}
+void deleteQuestion(Question bank[], int* count) {
+
+
+
+}
